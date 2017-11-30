335b1f813f03e116190e09f608b508e6af68f8fd
Isis van der Plas <isisvanderplas@hotmail.com>
2017-11-30 23:26:11 +0100

    add pug

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send("Hello world!");
+  res.send("<h1>Hello world!</h1>");
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+  res.send("<h1>Hello form hello!</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("the application is running on port 3000")
+});
